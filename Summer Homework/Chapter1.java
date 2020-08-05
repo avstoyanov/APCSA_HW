1ace57a890ec26542b731adf0706ba18db687dcf
Alexander Stoyanov <mushtty@gmail.com>
2020-08-05 17:57:10 -0400

    created project and completed hello world program

---
+++ Summer Homework/Chapter1.java
@@ -0,0 +1,12 @@
+public class Chapter1 {
+
+    public static void hello(){
+        System.out.println("Hello, World!");
+        System.out.println("How are you?"); //totally not witty
+    }
+
+    // Main method for calling the exercises (separate methods)
+    public static void main(String[] args) {
+        hello();
+    }
+}
---
+++ Summer Homework/Chapter2.java
@@ -0,0 +1,11 @@
+public class Chapter2 {
+
+    public static void hello(){
+        System.out.println("Hello World2!");
+    }
+
+    // Main metod for calling the exercises (separate methods)
+    public static void main(String[] args) {
+        hello();
+    }
+}
